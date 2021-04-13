76533b432bbe733fd74ca68668925f5a336068ea
cyber01 <cyberguy0526@gmail.com>
2021-04-13 19:14:50 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+;
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyeongmin-kim:Mrlagud12@boilerplate.bzfe6.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Conneted...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello, World!'))
+
+
+  app.get('/', (req, res) => res.send('안녕하세요!!'))
+  
+
+  app.post('/register', (req, res) => {
+
+    
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })  
+    })
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
